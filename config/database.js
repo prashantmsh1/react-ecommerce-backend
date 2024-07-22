e9551b6f446ea4e73713ccfea35b954554bf63d3
prashant <prashantkr.msh@gmail.com>
2024-07-22 06:16:20 +0530

    chore: Update database configuration for PostgreSQL port number

--- config/database.js
+++ config/database.js
@@ -57,9 +57,9 @@ module.exports = ({ env }) => {
       connection: {
         connectionString: env("DATABASE_URL"),
         host: env("DATABASE_HOST", "aws-0-ap-south-1.pooler.supabase.com"),
-        port: env.int("DATABASE_PORT", 6543),
+        port: env.int("DATABASE_PORT", 5432),
         database: env("DATABASE_NAME", "postgres"),
-        user: env("DATABASE_USERNAME", "postgres.aaslsdpjvtdneuidjfrj"),
+        user: env("DATABASE_USERNAME", "postgres"),
         password: env("DATABASE_PASSWORD", "Prashant@965308"),
         ssl: env.bool("DATABASE_SSL", false) && {
           key: env("DATABASE_SSL_KEY", undefined),
